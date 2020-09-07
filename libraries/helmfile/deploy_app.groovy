32a9e098771e6e8b4b96e4b50336af29b6fce4a6
Cedric Lamalle <cedric.lamalle@basis.com.br>
2020-09-06 22:27:02 -0300

    Add diff before deploying

--- libraries/helmfile/deploy_app.groovy
+++ libraries/helmfile/deploy_app.groovy
@@ -1,6 +1,10 @@
 void call(app_env) {
+    def runDiff = config.runDiff ?: true
     stage("Helmfile Deploy"){
-        println "Deploy from Helmfile from URL: ${config.configurationUrl} from Dir: ${app_env.configurationDir}"
+      if (runDiff) {
+        println "Running diff before applying..."
+      }
+      println "Deploy from Helmfile from URL: ${config.configurationUrl} from Dir: ${app_env.configurationDir}"
     }
 }
 
