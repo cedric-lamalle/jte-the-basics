892edc3b48d495a37b68a288a8560c1b2b90c733
Cedric Lamalle <cedric.lamalle@basis.com.br>
2020-09-10 00:01:49 -0300

    Add Stage Context to Build Output

--- libraries/maven/build.groovy
+++ libraries/maven/build.groovy
@@ -1,5 +1,9 @@
 void call(){
-    stage("Maven: Build"){
+    stage("Maven: Build") {
+        if (stageContext.name) {
+            echo "Running from ${stageContext.name}"
+            stageContext.args.each { entry -> println "${entry.key}=${entry.value}" }
+        }
         println "build from the maven library"
     }
 }
